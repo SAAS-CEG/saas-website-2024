e6f46e8ad8e114355def59da97bda45aa4bc2258
praveen-221 <praveen-221>
2024-02-02 15:31:39 +0530

    changes loader timing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -171,7 +171,7 @@
   //   $("#js-preloader").addClass("loaded");
   // });
   // Set a maximum duration for the loader (20 seconds)
-  const maxLoaderDuration = 10000;
+  const maxLoaderDuration = 15000;
   let loaderTimeout;
 
   // Function to hide the loader
